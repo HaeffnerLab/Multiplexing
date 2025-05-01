0ad352f153517498b39587abf6d0f193a73fee3e
Bingran You <bingran.you@berkeley.edu>
2025-05-01 15:34:18 -0700

    g2_data_processing/summed_caled_data_index.npy

--- g2_data_processing/III_combined_g2_processing_and_plotting.py
+++ g2_data_processing/III_combined_g2_processing_and_plotting.py
@@ -95,6 +95,7 @@ def load_and_plot_caled_data():
     
     # Plot summed data
     if summed_data is not None:
+        np.save('summed_caled_data_index.npy', index_data)
         np.save('summed_caled_data.npy', summed_data)
         plt.figure(figsize=(12, 8))
         plt.plot(index_data, summed_data)
